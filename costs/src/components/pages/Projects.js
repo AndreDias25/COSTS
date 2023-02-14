40a1fd169eeff35c257de8f779f5483e576c4477
André Dias <andre25dias@gmail.com>
2023-02-14 15:31:17 -0300

    dashboard de projetos

--- costs/src/components/pages/Projects.js
+++ costs/src/components/pages/Projects.js
@@ -1,5 +1,11 @@
 import { useLocation } from "react-router-dom"; //para pegar a mensagem do body do fetch que eu fiz no create Post no NewProject.js
 
+import styles from './Projects.module.css';
+
+import Container  from '../layout/Container';
+import LinkButton from '../layout/LinkButton';
+
+
 import Message from "../layout/Message";
 
 function Projects(){
@@ -11,9 +17,15 @@ function Projects(){
     }
     
     return (
-        <div>
-            <h1>Meus Projetos</h1>
+        <div className={styles.project_container}>
+            <div className={styles.title_container}>
+                <h1>Meus Projetos</h1>
+                <LinkButton to="/newproject" text="Criar Projeto"/>
+            </div>
             {message && <Message msg={message} type="sucess"/>}
+            <Container customClass="Projetos">
+                <p>Projetos...</p>
+            </Container>
         </div>
     )
 }
